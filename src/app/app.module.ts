b8bfb1f9a03dd56c2e5437b347988be396834023
Carlos Quimbay <quimbayc@javeriana.edu.co>
2019-11-21 19:02:17 -0600

    added apod service and updated imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,19 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { HttpClientModule } from '@angular/common/http';
+import { NgApodConfig } from '../../../config/ng-apod.config';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
+    NgApodConfig,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }

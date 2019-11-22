ba1e089c9581f6e4f29007f7b9a00300ad473a9b
Carlos Quimbay <quimbayc@javeriana.edu.co>
2019-11-21 19:26:59 -0600

    added apod page elements and logic

--- src/app/apod/apod.module.ts
+++ src/app/apod/apod.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -8,13 +9,24 @@ import { ApodPageRoutingModule } from './apod-routing.module';
 
 import { ApodPage } from './apod.page';
 
+import { SafePipeModule } from 'safe-pipe';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ApodPage
+  }
+];
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    SafePipeModule,
+    RouterModule.forChild(routes),
     ApodPageRoutingModule
   ],
   declarations: [ApodPage]
 })
-export class ApodPageModule {}
+export class ApodPageModule { }
